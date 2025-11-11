10d10a58984b1ddcdee87c04c22ceadc3e01f86c
HwangJeongSeong <hjs54336297@gmail.com>
2025-11-11 16:43:21 +0900

    err fix

--- src/main/java/com/ll/guardian/GuardianApplication.java
+++ src/main/java/com/ll/guardian/GuardianApplication.java
@@ -1,9 +1,14 @@
 package com.ll.guardian;
 
+import com.ll.guardian.domain.edrug.properties.EasyDrugProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableConfigurationProperties(EasyDrugProperties.class)
 public class GuardianApplication {
 
 	public static void main(String[] args) {

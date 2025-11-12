6ea8c9d977da0d545b3e3ad686dd9004e8abbc1b
HwangJeongSeong <hjs54336297@gmail.com>
2025-11-12 18:25:00 +0900

    mobile push

--- src/main/java/com/ll/guardian/global/config/SecurityConfig.java
+++ src/main/java/com/ll/guardian/global/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/chat/**",
+                                "/api/push/**",
                                 "/ws/**",
                                 "/topic/**",
                                 "/h2-console/**",

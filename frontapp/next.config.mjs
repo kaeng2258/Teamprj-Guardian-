dea30cafed8c6d03d244fe85b58dae11da5f3e54
깃íkaeng2258 <kaeng2258@gmail.com>
2025-12-04 15:22:00 +0900

    frontapp fix

--- frontapp/next.config.mjs
+++ frontapp/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    output: "standalone",
   reactStrictMode: true,
   /* config options here */
   async rewrites() {

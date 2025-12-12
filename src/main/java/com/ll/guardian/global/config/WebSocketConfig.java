45c9542f309856c25474e284a33efccba3fa0127
깃íkaeng2258 <kaeng2258@gmail.com>
2025-12-12 18:26:41 +0900

    fix

--- src/main/java/com/ll/guardian/global/config/WebSocketConfig.java
+++ src/main/java/com/ll/guardian/global/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
+        registry.addEndpoint("/ws-stomp")
             .setAllowedOriginPatterns(
                 "http://localhost:*",
                 "http://127.0.0.1:*",
